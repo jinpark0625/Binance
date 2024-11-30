470f976fc88c4370621e19af69cc92b2fc988aaa
jin park <jinpark0625@gmail.com>
2024-11-30 12:37:12 +0900

    feat: set up React Query and app state management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  focusManager,
+} from "@tanstack/react-query";
+import { AppStateStatus, Platform } from "react-native";
+import { useOnlineManager, useAppState } from "@hooks/query";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const onAppStateChange = (status: AppStateStatus) => {
+    if (Platform.OS !== "web") {
+      focusManager.setFocused(status === "active");
+    }
+  };
+
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { retry: 2 } },
+  });
+
+  useOnlineManager();
+  useAppState(onAppStateChange);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
