9524b6652dab04bfa24670474d690f8ca91e938e
jin park <jinpark0625@gmail.com>
2024-11-30 20:41:29 +0900

    feat: Update with new theme

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,11 +1,54 @@
-import { Text as RNText } from "react-native";
+import {
+  Text as RNText,
+  type TextProps,
+  TextStyle,
+  StyleSheet,
+} from "react-native";
+import { useThemeColor } from "@/hooks/theme/useThemeColor";
 
-interface Text {
+interface Text extends TextProps {
+  variant?: "xxs" | "xs" | "s" | "m" | "lg";
+  color?: "textPrimary" | "textSecondary";
   children: string;
 }
 
-export const Text = ({ children }: Text) => {
-  return <RNText>{children}</RNText>;
+const Text = ({
+  variant = "m",
+  color = "textPrimary",
+  style,
+  children,
+}: Text) => {
+  const themeColor = useThemeColor({}, color);
+
+  const textStyles: TextStyle = {
+    ...styles[variant],
+    color: themeColor,
+  };
+
+  return <RNText style={[textStyles, style]}>{children}</RNText>;
 };
 
 export default Text;
+
+const styles = StyleSheet.create({
+  xxs: {
+    fontSize: 10,
+    lineHeight: 14,
+  },
+  xs: {
+    fontSize: 12,
+    lineHeight: 18,
+  },
+  s: {
+    fontSize: 14,
+    lineHeight: 21,
+  },
+  m: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  lg: {
+    fontSize: 18,
+    lineHeight: 27,
+  },
+});
