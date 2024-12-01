0c24de5e2be0ff15b04e7dd7c48dc3a9c949f4f1
jin park <jinpark0625@gmail.com>
2024-12-01 14:09:17 +0900

    refactor: simplify useThemeColor hook

--- hooks/theme/useThemeColor.ts
+++ hooks/theme/useThemeColor.ts
@@ -1,16 +1,8 @@
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "./useColorScheme";
 
-export function useThemeColor(
-  props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-) {
+export function useThemeColor() {
   const theme = useColorScheme() ?? "light";
-  const colorFromProps = props[theme];
 
-  if (colorFromProps) {
-    return colorFromProps;
-  } else {
-    return Colors[theme][colorName];
-  }
+  return Colors[theme];
 }
