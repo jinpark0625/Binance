157436016fb9bf813bb0270073a0a682b237a40c
jin park <jinpark0625@gmail.com>
2024-12-01 22:50:40 +0900

    feat: enhance Button component with flexible styling options

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,61 @@
-import { Pressable } from "react-native";
+import { ReactNode } from "react";
+import {
+  Pressable,
+  PressableProps,
+  StyleSheet,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
 import Text from "./Text";
+import { palette } from "@/constants/Colors";
 
-interface Button {
-  title: string;
+interface Button extends PressableProps {
+  title?: string;
   onPress: () => void;
   disabled?: boolean;
+  variant?: "primary" | "secondary" | "custom";
+  style?: StyleProp<ViewStyle>;
+  children?: ReactNode;
 }
 
-const Button = ({ title, onPress, disabled = false }: Button) => {
+const Button = ({
+  title,
+  onPress,
+  disabled = false,
+  variant = "primary",
+  style,
+  children,
+}: Button) => {
+  const isCustom = variant === "custom";
+
   return (
-    <Pressable onPress={onPress} disabled={disabled}>
-      <Text>{title}</Text>
+    <Pressable
+      onPress={onPress}
+      disabled={disabled}
+      style={[
+        !isCustom && styles.container,
+        !isCustom && styles[variant],
+        style,
+      ]}
+    >
+      {title ? <Text staticColor="white">{title}</Text> : children}
     </Pressable>
   );
 };
 
 export default Button;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 36,
+    borderRadius: 8,
+  },
+  primary: {
+    backgroundColor: palette.green,
+  },
+  secondary: {
+    backgroundColor: palette.red,
+  },
+});
