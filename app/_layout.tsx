672044dc9c9116eef0427130c041664bff03963e
jin park <jinpark0625@gmail.com>
2024-12-02 13:33:42 +0900

    feat: add theme provider with light and dark mode support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,18 @@ import {
   QueryClientProvider,
   focusManager,
 } from "@tanstack/react-query";
-import { AppStateStatus, Platform, View } from "react-native";
+import { AppStateStatus, Platform, View, useColorScheme } from "react-native";
 import { useOnlineManager, useAppState } from "@/hooks/query";
 import { GlobalBottomSheet } from "@/components";
 import { Stack } from "expo-router";
 import { RecoilRoot } from "recoil";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { ThemeProvider } from "@react-navigation/native";
+import { lightTheme, darkTheme } from "@/constants/Colors";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   const onAppStateChange = (status: AppStateStatus) => {
     if (Platform.OS !== "web") {
       focusManager.setFocused(status === "active");
@@ -25,18 +29,20 @@ export default function RootLayout() {
   useAppState(onAppStateChange);
 
   return (
-    <GestureHandlerRootView>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <View style={{ flex: 1 }}>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="+not-found" />
-            </Stack>
-            <GlobalBottomSheet />
-          </View>
-        </RecoilRoot>
-      </QueryClientProvider>
-    </GestureHandlerRootView>
+    <ThemeProvider value={colorScheme === "dark" ? darkTheme : lightTheme}>
+      <GestureHandlerRootView>
+        <QueryClientProvider client={queryClient}>
+          <RecoilRoot>
+            <View style={{ flex: 1 }}>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="+not-found" />
+              </Stack>
+              <GlobalBottomSheet />
+            </View>
+          </RecoilRoot>
+        </QueryClientProvider>
+      </GestureHandlerRootView>
+    </ThemeProvider>
   );
 }
