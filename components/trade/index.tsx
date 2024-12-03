c19374417c0e4dfc51c1c0ef42c9ba89162eb002
jin park <jinpark0625@gmail.com>
2024-12-03 22:04:19 +0900

    feat: update module export for new components

---
+++ components/trade/index.tsx
@@ -0,0 +1,6 @@
+export { default as CoinInfo } from "./CoinInfo";
+export { default as OrdersTab } from "./OrdersTab";
+export { default as PriceIndicator } from "./PriceIndicator";
+export { default as TradeForm } from "./TradeForm";
+export { default as TradeTab } from "./TradeTab";
+export { default as TradeOverview } from "./TradeOverview";
