b1826506bc85316e72c887a2350674fb4965d837
jin park <jinpark0625@gmail.com>
2024-12-04 13:21:40 +0900

    fix: adjust Android header padding

--- components/SearchHeader.tsx
+++ components/SearchHeader.tsx
@@ -1,18 +1,25 @@
-import { View, SafeAreaView, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import SearchBar from "./SearchBar";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { router } from "expo-router";
 import { useThemeColor } from "@/hooks/theme/useThemeColor";
 import { useSetRecoilState } from "recoil";
 import { searchQueryState } from "@/atom/searchQueryAtom";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const SearchHeader = () => {
+  const inset = useSafeAreaInsets();
   const colorTheme = useThemeColor();
 
   const setSearchQuery = useSetRecoilState(searchQueryState);
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        paddingTop: Platform.OS === "ios" ? inset.top : inset.top + 12,
+        ...styles.container,
+      }}
+    >
       <MaterialIcons
         name="arrow-back"
         size={24}
@@ -22,7 +29,7 @@ const SearchHeader = () => {
       <View style={{ flex: 1 }}>
         <SearchBar isHeader={true} onChangeText={setSearchQuery} />
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
